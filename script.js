cf1a317763f2c1c248169a3cc0afc0d9ede916e7
Kayla Ingram <kaylabsi@outlook.com>
2020-10-20 09:36:30 -0400

    modified minor code errors

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const name = "Kayla Ingram"
 let age = 26;
 const birthday = "November 1st"
@@ -12,19 +14,19 @@ if (detroitGC === true) {
     console.log (`My name is ${name} and I am a student at Grand Circus in Grand Rapids, Michigan. I am currently ${age} years old and my birthday is on ${birthday}.`);
 }
 
-for(i = 0; i < lifeEvents.length; i++){
+for(let i = 0; i < lifeEvents.length; i++){
     console.log (lifeEvents[i]);
 }
 
 let counter = 0;
 
 while (true) {
-    let randomNumber = (Math.floor(Math.random() * 11));
+    let randomNumber = (Math.floor(Math.random() * (10) + 1));
 if (randomNumber!==5) {
     counter++;
     console.log (`${randomNumber} !==5`);
 } else {
     counter++;
     console.log (`5===5. It took ${counter} iterations to randomly generate the number 5`);
-break;}
+    break;} 
 } 
